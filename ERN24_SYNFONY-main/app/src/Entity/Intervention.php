ea9920f4c3be85f561e0a4724c9477cc68659e8c
Taibi-Salah <taibi.saladin@proton.me>
2024-07-06 21:04:52 +0200

    Add .gitattributes file to ensure consistent line endings

---
+++ ERN24_SYNFONY-main/app/src/Entity/Intervention.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\InterventionRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: InterventionRepository::class)]
+class Intervention
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $actionTaken = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $date = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getActionTaken(): ?string
+    {
+        return $this->actionTaken;
+    }
+
+    public function setActionTaken(string $actionTaken): static
+    {
+        $this->actionTaken = $actionTaken;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeInterface $date): static
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+}
