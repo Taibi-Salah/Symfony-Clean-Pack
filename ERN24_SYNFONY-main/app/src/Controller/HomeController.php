4bd906f4d3953106db82420f31dd5a8b07d8087f
loic <loic.rossignol66@gmail.com>
2024-07-08 13:10:37 +0200

    dashboard

--- ERN24_SYNFONY-main/app/src/Controller/HomeController.php
+++ ERN24_SYNFONY-main/app/src/Controller/HomeController.php
@@ -15,4 +15,22 @@ class HomeController extends AbstractController
             'controller_name' => 'HomeController',
         ]);
     }
+#[Route('/dashboard', name: 'app_dashboard')]
+    public function tableauDeBord(): Response
+    {
+        // Ces données devraient venir de votre logique métier / base de données
+        $data = [
+            'openTickets' => 25,
+            'inProgressTickets' => 15,
+            'resolvedToday' => 10,
+            'avgResolutionTime' => 4.5,
+            'recentTickets' => [
+                ['title' => 'Problème de connexion', 'date' => new \DateTime()],
+                ['title' => 'Erreur 404', 'date' => new \DateTime('-1 day')],
+                ['title' => 'Mise à jour nécessaire', 'date' => new \DateTime('-2 days')],
+            ],
+        ];
+
+        return $this->render('home/dashboard.html.twig', $data);
+    }
 }
