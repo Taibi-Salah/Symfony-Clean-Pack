cd5936ff27a072e09432a4bbdb3ed59a34909265
Format_Pro <fgalinier51@gmail.com>
2024-07-08 13:10:17 +0200

    ajout des formulaires

--- ERN24_SYNFONY-main/app/src/Controller/HomeController.php
+++ ERN24_SYNFONY-main/app/src/Controller/HomeController.php
@@ -8,6 +8,12 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class HomeController extends AbstractController
 {
+
+    /**
+     * Route qui permet d'afficher la page d'accueil
+     * @Route("/", name="app_home")
+     * @return Response
+     */
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
@@ -15,4 +21,33 @@ class HomeController extends AbstractController
             'controller_name' => 'HomeController',
         ]);
     }
+
+
+    /**
+     * Route qui permet de se connecter
+     * @Route("/connexion", name="app_connexion")
+     * @return Response
+     */
+    #[Route('/connexion', name: 'app_connexion')]
+    public function login(): Response
+    {
+        return $this->render('home/connexion/connexion.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+
+    /**
+     * Route qui permet de s'inscrire
+     * @Route("/inscription", name="app_inscription")
+     * @return Response
+     */
+    #[Route('/inscription', name: 'app_inscription')]
+    public function register(): Response
+    {
+        return $this->render('home/connexion/inscription.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+
+
 }
