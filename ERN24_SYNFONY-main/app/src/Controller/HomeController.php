0c2a2649c98a94be7f3fa6f245c700ad0c08cda8
Format_Pro <fgalinier51@gmail.com>
2024-07-08 13:22:04 +0200

    ajout usercontroller

--- ERN24_SYNFONY-main/app/src/Controller/HomeController.php
+++ ERN24_SYNFONY-main/app/src/Controller/HomeController.php
@@ -23,31 +23,5 @@ class HomeController extends AbstractController
     }
 
 
-    /**
-     * Route qui permet de se connecter
-     * @Route("/connexion", name="app_connexion")
-     * @return Response
-     */
-    #[Route('/connexion', name: 'app_connexion')]
-    public function login(): Response
-    {
-        return $this->render('home/connexion/connexion.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
-    }
-
-    /**
-     * Route qui permet de s'inscrire
-     * @Route("/inscription", name="app_inscription")
-     * @return Response
-     */
-    #[Route('/inscription', name: 'app_inscription')]
-    public function register(): Response
-    {
-        return $this->render('home/connexion/inscription.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
-    }
-
 
 }
