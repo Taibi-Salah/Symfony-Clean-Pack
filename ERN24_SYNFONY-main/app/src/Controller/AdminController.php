17fee99e460b5126302af775c8f0a2cff30bfb56
Taibi-Salah <Taibi-Salah>
2024-07-08 13:22:59 +0200

    AdminDashboard OK , 2 New Entity [Admin;User]

---
+++ ERN24_SYNFONY-main/app/src/Controller/AdminController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\UserRepository;
+
+class AdminController extends AbstractController
+{
+    #[Route('/admin', name: 'admin_dashboard')]
+    public function index(UserRepository $userRepository): Response
+    {
+        // Retrieve all users from the database
+        $users = $userRepository->findAll();
+
+        return $this->render('admin/index.html.twig', [
+            'users' => $users,
+        ]);
+    }
+}
+
+
+
+
