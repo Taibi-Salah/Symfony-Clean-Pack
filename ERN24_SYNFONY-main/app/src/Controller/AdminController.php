8d43dd29d6e3ad39b2c2ee65f160a40c5731c811
loic <loic.rossignol66@gmail.com>
2024-07-10 11:58:34 +0200

    filtre

--- ERN24_SYNFONY-main/app/src/Controller/AdminController.php
+++ ERN24_SYNFONY-main/app/src/Controller/AdminController.php
@@ -7,6 +7,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\UserRepository;
 
+
 class AdminController extends AbstractController
 {
     #[Route('/admin', name: 'admin_dashboard')]
