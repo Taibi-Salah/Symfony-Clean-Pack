ed07893ab4a628f75326de4fbc3f81bc272b2877
Format_Pro <fgalinier51@gmail.com>
2024-07-10 14:09:05 +0200

    conflit

--- ERN24_SYNFONY-main/app/src/Form/LoginType.php
+++ ERN24_SYNFONY-main/app/src/Form/LoginType.php
@@ -5,10 +5,11 @@
 namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 
 class LoginType extends AbstractType
 {
@@ -20,8 +21,10 @@ class LoginType extends AbstractType
                 'attr' => ['placeholder' => 'Your email'],
             ])
             ->add('password', PasswordType::class, [
-                'label' => 'Password',
-                'attr' => ['placeholder' => 'Your password'],
+                'label' => 'Password', 
+            ])
+            ->add('login', SubmitType::class, [
+                'label' => 'Se connecter',
             ]);
     }
 
