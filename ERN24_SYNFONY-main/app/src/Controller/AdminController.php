1f411fa2a74cfb0277b7f232d38003d0eccb8042
Format_Pro <fgalinier51@gmail.com>
2024-07-10 13:53:18 +0200

    ajout style login + ajout slash dev linkbase

--- ERN24_SYNFONY-main/app/src/Controller/AdminController.php
+++ ERN24_SYNFONY-main/app/src/Controller/AdminController.php
@@ -7,7 +7,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\UserRepository;
 
-#[Route('/admin')]
+
 class AdminController extends AbstractController
 {
     #[Route('/admin', name: 'admin_dashboard')]
--- ERN24_SYNFONY-main/app/src/Controller/UserController.php
+++ ERN24_SYNFONY-main/app/src/Controller/UserController.php
@@ -4,11 +4,13 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\UserType;
+use App\Form\LoginType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class UserController extends AbstractController
@@ -19,6 +21,68 @@ class UserController extends AbstractController
     {
         $this->entityManager = $entityManager;
     }
+
+    #[Route('/connexion', name: 'app_connexion')]
+    public function login(AuthenticationUtils $authenticationUtils, Request $request, UserPasswordHasherInterface $passwordHasher): Response
+    {
+        // If user is already logged in, redirect them to dashboard
+        if ($this->getUser()) {
+            return $this->redirectToRoute('app_dashboard');
+        }
+
+        $form = $this->createForm(LoginType::class);
+        $form->handleRequest($request);
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Retrieve form data (as an object of Client entity)
+            /** @var Client $formData */
+            $formData = $form->getData();
+
+            // Get the email entered in the form
+            $email = $formData->getEmail();
+
+            // Find the client by email in the database
+            $client = $this->entityManager->getRepository(Client::class)->findOneBy(['email' => $email]);
+
+            if (!$client) {
+                // Handle non-existent client (email not found)
+                $this->addFlash('error', 'Email non trouvé.');
+                return $this->redirectToRoute('app_connexion');
+            }
+
+            // Check if the password is correct using UserPasswordHasherInterface
+            if ($passwordHasher->isPasswordValid($client, $formData->getPassword())) {
+                // Password is valid, proceed with authentication
+                // This part is usually handled by Symfony's security system automatically
+
+                // Redirect to dashboard upon successful login
+                return $this->redirectToRoute('app_dashboard');
+            } else {
+                // Handle incorrect password
+                $this->addFlash('error', 'Mot de passe incorrect.');
+                return $this->redirectToRoute('app_connexion');
+            }
+        }
+
+        return $this->render('home/connexion/connexion.html.twig', [
+            'form' => $form->createView(),
+            'last_username' => $lastUsername,
+            'error' => $error,
+        ]);
+    }
+
+    // #[Route('/dashboard', name: 'app_dashboard')]
+    // public function dashboard(): Response
+    // {
+    //     // You can add logic here to fetch data or perform actions needed for the dashboard
+    //     return $this->render('dashboard.html.twig');
+    // }
 
     #[Route('/inscription', name: 'app_inscription')]
     public function register(Request $request, UserPasswordHasherInterface $passwordHasher): Response
