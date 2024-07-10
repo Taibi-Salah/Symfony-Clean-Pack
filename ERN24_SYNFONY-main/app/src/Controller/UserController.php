f9353ac382db08d7743f2b3f47f66d9f8331615a
Format_Pro <fgalinier51@gmail.com>
2024-07-10 16:18:03 +0200

    ajout formulaire inscription mais pas encore fini

--- ERN24_SYNFONY-main/app/src/Controller/UserController.php
+++ ERN24_SYNFONY-main/app/src/Controller/UserController.php
@@ -6,6 +6,7 @@ use App\Entity\User;
 use App\Form\UserType;
 use App\Form\LoginType;
 use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -85,29 +86,31 @@ class UserController extends AbstractController
     // }
 
     #[Route('/inscription', name: 'app_inscription')]
-    public function register(Request $request, UserPasswordHasherInterface $passwordHasher): Response
-    {
-        $user = new User();
-        $form = $this->createForm(UserType::class, $user);
-        
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            // Encode the plain password before storing
-            $hashedPassword = $passwordHasher->hashPassword($user, $user->getPlainPassword());
-            $user->setPassword($hashedPassword);
-
-            $this->entityManager->persist($user);
-            $this->entityManager->flush();
-
-            // Redirect to login page after successful registration
-            return $this->redirectToRoute('app_connexion');
+    public function register(Request $request, UserPasswordHasherInterface $passwordHasher, ManagerRegistry $managerRegistry): Response
+    {  
+        if ($this->getUser()) {
+        return $this->redirectToRoute('app_dashboard');
         }
+        $user = new User();
 
-        return $this->render('home/connexion/inscription.html.twig', [
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $em= $managerRegistry->getManager();
+            $em->persist($form->getData($user));
+            $em->flush();
+            $this->addFlash('success', 'Votre compte a bien été créé');
+            return $this->redirectToRoute('app_dashboard');
+        }
+       
+
+        return $this->render("home/connexion/inscription.html.twig", [
             'form' => $form->createView(),
+
+
         ]);
     }
+
 
     #[Route('/mytickets', name: 'app_tickets')]
     public function tickets(): Response
---
+++ ERN24_SYNFONY-main/app/src/Form/UserType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Form;
+
+use Assert\Email;
+use App\Entity\User;
+use Assert\NotBlank;
+use App\Entity\ContactInformation;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email', EmailType::class, 
+            )
+            ->add('Password', PasswordType::class)
+            
+            ->add('inscription', SubmitType::class, [
+                'label' => 'Se connecter',
+            ]);
+
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
